53a05ef4cf35648818520d81c3aad67a8dfe8ddc
Nagesh Mishra <nagesh3.13@gmail.com>
2022-03-24 20:38:50 +0530

    Updates

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SearchLocation } from "./Home";
-
-function App() {
-  return <SearchLocation></SearchLocation>;
-}
-
-export default App;
--- src/util/service.ts
+++
@@ -1,4 +0,0 @@
-export const d = {};
-export const capitalizeFirstLetter = (word: any) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
